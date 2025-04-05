07e7c1ae1fdb4b5ae7d9315994cff0b214d56caf
thiago andrade nascimento <165865860+Thiagoblackphotos@users.noreply.github.com>
2025-04-05 14:56:12 -0300

    Update ProvaMedia.java

--- ProvaJava/ProvaMedia.java
+++ ProvaJava/ProvaMedia.java
@@ -4,29 +4,45 @@ public class ProvaMedia {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double prova1 = 0.0;
+        double prova2;
+        double prova3;
+        double prova4;
+        double media;
+        double notaMinima;
+
+
+
+
         System.out.println("Calcule a sua média");
 
+        System.out.println("Digite a nota minima:");
+        notaMinima = scanner.nextDouble();
+        
+
         System.out.println("Digite a nota da AC1:");
-        double prova1 = scanner.nextDouble() * 0.15;
+        prova1 = scanner.nextDouble();
 
         System.out.println("Digite a nota da AC2:");
-        double prova2 = scanner.nextDouble() * 0.30;
+        prova2 = scanner.nextDouble();
 
-        System.out.println("Digite a nota da AC3:");
-        double prova3 = scanner.nextDouble() * 0.10;
+        System.out.println("Digite a nota da AG:");
+        prova3 = scanner.nextDouble();
 
         System.out.println("Digite a nota da AF:");
-        double prova4 = scanner.nextDouble() * 0.45; 
+        prova4 = scanner.nextDouble(); 
+
         
-        double media = prova1 + prova2 + prova3 + prova4;
+        
+        media = prova1 * 0.15 + prova2 * 0.30 + prova3 * 0.10 + prova4 * 0.45;
 
         System.out.println("Sua média é: " + media);
 
-        if (media >= 6) {
+        if (notaMinima <= media) {
             System.out.println("Aprovado!");
             System.out.println("AC1: " + prova1);
             System.out.println("AC2: " + prova2);
-            System.out.println("AC3: " + prova3);
+            System.out.println("AG: " + prova3);
             System.out.println("AF: " + prova4);
         } else {
             System.out.println("Reprovado!");
